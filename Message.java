27a1756b531da6b75d296081be6e84e8112ed2eb
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 18:06:50 -0500

    Initial commit

---
+++ Message.java
@@ -0,0 +1,9 @@
+public class Message {
+	public static void main(String[] args){
+		System.out.println("Bibek Gupta");
+		System.out.println("We Learned: Java is very old language and still used on almost every application in the world");
+		System.out.println("Java is very useful. GUI is used on Java. Java is used on Android.");
+		System.out.println("First phone in the world also used Java. Java is like very basic language which every");
+		System.out.println("one should learn who want to come into Computer Science World.");
+	}
+}
